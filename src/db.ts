ad20f3ed061cf4a2c07e2b90eae9e157df456d6b
Luis2115 <lmorochofebres@gmail.com>
2022-06-27 01:45:42 -0500

    Organizando proyecto

--- src/db.ts
+++ src/db.ts
@@ -1,22 +1,24 @@
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm";
+import { Users } from "./Entities/User";
 
 export const connectDB = async () => {
-    await new DataSource({
-        type: "mysql",
-        host: "localhost",
-        port: 3306,
-        username: "root",
-        password: "",
-        database: "prueba",
-        synchronize: false,
-        logging: false,
-        entities:[],
-        migrations:[],
-        subscribers:[],
-        ssl: false,   
-        multipleStatements: true,
-    }).initialize().then(() => {
-        console.log("DB connected");
+  await new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "prueba",
+    synchronize: true, // Auto create tables
+    logging: false,
+    entities: [Users], // Entities to load in DB
+    migrations: [],
+    subscribers: [],
+    ssl: false,
+    multipleStatements: true,
+  })
+    .initialize()
+    .then(() => {
+      console.log("DB connected");
     });
-}
-    
+};
