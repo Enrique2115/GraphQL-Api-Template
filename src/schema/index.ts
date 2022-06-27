5aafee23dd42f2087965a38fb98dd6ca4a5de5ee
Luis2115 <lmorochofebres@gmail.com>
2022-06-27 01:53:11 -0500

    Agregando queries y mutaciones al schema de graphql

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,17 +1,34 @@
-import { GraphQLObjectType, GraphQLSchema } from "graphql"
-import { GREETING } from "./Queries/Greeting"
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
+import { GREETING } from "./Queries/Greeting";
+import { GET_ALL_USERS, GET_USER_BY_ID } from "./Queries/User";
+import { CREATE_USER, DELETE_USER, UPDATE_USER } from "./Mutations/User";
 
-/* Creando un nuevo GraphQLObjectType llamado RootQuery. */
+/**
+ *  El RootQuery es el que se encarga de recibir las peticiones de los clientes(consultas base).
+ */
 const RootQuery = new GraphQLObjectType({
-    name: "RootQuery",
-    fields: {
-        greettings: GREETING
-    }
-})
+  name: "RootQuery",
+  fields: {
+    greettings: GREETING,
+    getAllUsers: GET_ALL_USERS,
+    getUserById: GET_USER_BY_ID,
+  },
+});
+
+/**
+ * Mutation seran todos los metodos que se van a ejecutar en el servidor de mysql
+ */
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: CREATE_USER,
+    deleteUser: DELETE_USER,
+    updateUser: UPDATE_USER,
+  },
+});
 
 /* Creando un nuevo objeto GraphQLSchema. */
 export const schema = new GraphQLSchema({
-    query: RootQuery,
-    // mutation:{}
-})
-
+  query: RootQuery,
+  mutation: Mutation,
+});
