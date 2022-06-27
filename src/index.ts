a6269ca6e050f0924ec8e307e13fa9b1353457b3
Luis2115 <lmorochofebres@gmail.com>
2022-06-27 02:16:20 -0500

    Actualizando la variable port para express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import app from './app';
-import { connectDB } from './db';
+import app from "./app";
+import { connectDB } from "./db";
+import { PORT } from "./config";
 
-async function main(){
-    await connectDB();    
-    
-    app.listen(3000);
-    console.log('Server running on port 3000');
+async function main() {
+  await connectDB();
+
+  app.listen(PORT);
+  console.log("Server running on port 3000");
 }
 
-main()
+main();
